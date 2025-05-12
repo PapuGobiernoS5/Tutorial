7af5731fcc684645034b294423e7e0d92e2fb050
PapuGobiernoS5 <don.tony.stark.xd@gmail.com>
2025-05-11 19:40:12 -0600

    Initial commit

---
+++ src/main/java/com/tonystkssj/tutorial/Tutorial.java
@@ -0,0 +1,17 @@
+package com.tonystkssj.tutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tutorial implements ModInitializer {
+	public static final String MOD_ID = "tutorial";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
